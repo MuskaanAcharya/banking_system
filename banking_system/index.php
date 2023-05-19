aa4d7e03dbe81742d8e60204293d128e70964b70
MuskaanAcharya <128581857+MuskaanAcharya@users.noreply.github.com>
2023-05-19 19:59:32 +0530

    Add files via upload

---
+++ banking_system/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+		<title>BASIC BANK SYSTEM BY MUSKAAN</title>
+		 <link rel="stylesheet" href="style.css">
+		 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	</head>
+<body>
+	
+
+<div class="navbar">
+  <div class="logo">
+    <img src="7.jpg" id="logoimg" height="60px" width="70px">
+  </div>
+
+<ul id="navlist">
+  <li class="navlist"><a class='active' href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+  <li class="navlist"><a href="customers.php"><i class='fa fa-users' aria-hidden='true'></i> View Customers</a></li>
+</ul>
+</div>
+
+
+  <div class="dark-overlay">
+  	<div>
+    <h1 class="title" style="color:white">Parameters of this website</h1>
+
+      <div class="subtitle" style="color:white">Basic Banking System is a simple dynamic website,Easy Online Money Transfer</br></div>
+	  <div class="subtitle" style="color:white">No Login Page.No User Creation.Only tranfer of mney between multiple Users.</br></div>
+	  <div class="subtitle" style="color:white">Flow:Home page>View all customers>Select and View one Customers>Transfer Money>Select customer to transfer to>View all Customers.<br></div>
+        <a href="customers.php" class="button1"><b>Let's Get Started</b></a>
+  </div>
+
+
+ </div>
+<footer>
+  <p>© Developed by Muskaan Acharya</p><br>
+  	<ul class="foot">
+  	Contact Me:
+   <li><a target="_blank" class="github" href="https://github.com/MuskaanAcharya"><i class="fa fa-github fa-2x"></i></a></li>	
+   <li><a target="_blank" class="linkedin" href="https://www.linkedin.com/in/muskaan-acharya-597178246/"><i class="fa fa-linkedin fa-2x"></i></a></li> 
+</ul>
+
+</footer>
+
+
+ </body>
+	 </html>
